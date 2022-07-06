cb8c400372497d970fea77a4bce30c3fe104ac78
caleb <caleb@theradicalself.com>
2022-07-06 08:59:40 -0600

    Move appbar to RandomWords component, add new page icon

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -10,15 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Startup Name Generator',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Startup Name Generator'),
-        ),
-        body: const Center(child: RandomWords()),
-      ),
-    );
+    return const MaterialApp(
+        title: 'Startup Name Generator', home: RandomWords());
   }
 }
 
@@ -34,35 +27,49 @@ class _RandomWordsState extends State<RandomWords> {
   final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
 
+  void _pushSaved() {}
+
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (context, i) {
-      if (i.isOdd) return const Divider();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Startup Name Suggestions"),
+        actions: [
+          IconButton(
+            onPressed: _pushSaved,
+            icon: const Icon(Icons.list),
+            tooltip: "Saved Suggestions",
+          )
+        ],
+      ),
+      body: ListView.builder(itemBuilder: (context, i) {
+        if (i.isOdd) return const Divider();
 
-      final index = i ~/ 2;
-      if (index >= _suggestions.length) {
-        _suggestions.addAll(generateWordPairs().take(10));
-      }
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
 
-      final alreadySaved = _saved.contains(_suggestions[index]);
+        final alreadySaved = _saved.contains(_suggestions[index]);
 
-      return ListTile(
-        title: Text(_suggestions[index].asPascalCase, style: _biggerFont),
-        trailing: Icon(
-          alreadySaved ? Icons.favorite : Icons.favorite_border,
-          color: alreadySaved ? Colors.red : null,
-          semanticLabel: alreadySaved ? "Removed from Saved" : "Save",
-        ),
-        onTap: () {
-          setState(() {
-            if (alreadySaved) {
-              _saved.remove(_suggestions[index]);
-            } else {
-              _saved.add(_suggestions[index]);
-            }
-          });
-        },
-      );
-    });
+        return ListTile(
+          title: Text(_suggestions[index].asPascalCase, style: _biggerFont),
+          trailing: Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null,
+            semanticLabel: alreadySaved ? "Removed from Saved" : "Save",
+          ),
+          onTap: () {
+            setState(() {
+              if (alreadySaved) {
+                _saved.remove(_suggestions[index]);
+              } else {
+                _saved.add(_suggestions[index]);
+              }
+            });
+          },
+        );
+      }),
+    );
   }
 }
