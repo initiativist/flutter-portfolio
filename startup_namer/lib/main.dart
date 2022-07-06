0e7a373fdddf0398098a0a405dff28fb1c2fe343
caleb <caleb@theradicalself.com>
2022-07-06 08:43:36 -0600

    add heart icons for favoriting

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -16,9 +16,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: const Text('Startup Name Generator'),
         ),
-        body: const Center(
-          child: RandomWords()
-        ),
+        body: const Center(child: RandomWords()),
       ),
     );
   }
@@ -33,26 +31,29 @@ class RandomWords extends StatefulWidget {
 
 class _RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = const TextStyle(fontSize: 18);
-
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemBuilder: (context, i) {
-          if (i.isOdd) return const Divider();
+    return ListView.builder(itemBuilder: (context, i) {
+      if (i.isOdd) return const Divider();
 
-          final index = i ~/ 2;
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10));
-          }
-          return ListTile(
-            title: Text(
-              _suggestions[index].asPascalCase,
-              style: _biggerFont
-            )
-          );
-        }
-    );
+      final index = i ~/ 2;
+      if (index >= _suggestions.length) {
+        _suggestions.addAll(generateWordPairs().take(10));
+      }
+
+      final alreadySaved = _saved.contains(_suggestions[index]);
+
+      return ListTile(
+        title: Text(_suggestions[index].asPascalCase, style: _biggerFont),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+          semanticLabel: alreadySaved ? "Removed from Saved" : "Save",
+        ),
+      );
+    });
   }
 }
